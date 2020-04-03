e8da02ad32d562c879c6b4569f3079503e7664a3
JOSE LUIS TOVAR <jltovarg@gmail.com>
2020-04-03 16:16:40 -0500

    Ajuste mensajes Menus

--- src/main/java/co/edu/itp/ciecyt/web/rest/MenuUserResource.java
+++ src/main/java/co/edu/itp/ciecyt/web/rest/MenuUserResource.java
@@ -2,11 +2,13 @@ package co.edu.itp.ciecyt.web.rest;
 
 import java.text.MessageFormat;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
@@ -32,87 +34,104 @@ import io.github.jhipster.web.util.PaginationUtil;
 @RequestMapping("/api")
 public class MenuUserResource {
 
-    private final Logger log = LoggerFactory.getLogger(MenuResource.class);
+	private final Logger log = LoggerFactory.getLogger(MenuResource.class);
 
-    private static final String ENTITY_NAME = "menu";
-    private final UserService userService;
+	private static final String ENTITY_NAME = "menu";
+	private final UserService userService;
 
-    @Value("${jhipster.clientApp.name}")
-    private String applicationName;
+	@Value("${jhipster.clientApp.name}")
+	private String applicationName;
 
-  
+	private final MessageSource messageSource;
 
-    private final MenuService menuService; //agregada ADR
+	private final MenuService menuService; //agregada ADR
 
-    public MenuUserResource(MenuService menuService,  UserService userService) {
-      
-        this.menuService = menuService;
-        this.userService = userService;
-    }
+	public MenuUserResource(MenuService menuService,  UserService userService, MessageSource messageSource) {
 
-    
-
-    //ADR
-    /**
-     * {@code GET  /menus} : get all the menus.
-     *
-
-     * @param pageable the pagination information.
-
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of menus in body.
-     */
-    @GetMapping("/menus-all-user")
-    //public ResponseEntity<List<MenuDTO>> getAllMenusUser(@PathVariable Long id, Pageable pageable) {
-    public ResponseEntity<?> getAllMenusUser(Pageable pageable) {
-        log.debug("REST request to get a page of Menus of User");
-        try{
-        Optional<User> user = userService.getUserWithAuthorities();
-        //Page<MenuDTO> page = menuService.findAll(pageable);
-        Page <MenuDTO>  page = menuService.buscarAllByUser(user.get().getId(), pageable);
-        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
-        return ResponseEntity.ok().headers(headers).body(page.getContent());
-        }
-        catch(Exception e){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body( e.getMessage());
-
-        }
-    }
+		this.menuService = menuService;
+		this.userService = userService;
+		this.messageSource = messageSource;
+	}
 
 
-    //ADR
-    /**
-     * {@code GET  /menus} : get all the menus.
-     *
 
-     * @param pageable the pagination information.
+	//ADR
+	/**
+	 * {@code GET  /menus} : get all the menus.
+	 *
 
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of menus in body.
-     */
+	 * @param pageable the pagination information.
 
-   
-    @GetMapping("/menus-all-user-no-page")
-    //public ResponseEntity<List<MenuDTO>> getAllMenusUser(@PathVariable Long id, Pageable pageable) {
-    public ResponseEntity<?> getAllMenusUserNoPage() {
-        log.debug("REST request to get a page of Menus of User");
-        Optional<User> user = userService.getUserWithAuthorities();
-        try {
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of menus in body.
+	 */
+	@GetMapping("/menus-all-user")
+	//public ResponseEntity<List<MenuDTO>> getAllMenusUser(@PathVariable Long id, Pageable pageable) {
+	public ResponseEntity<?> getAllMenusUser(Pageable pageable) {
+		log.debug("REST request to get a page of Menus of User");
+		
+		Optional<User> user = userService.getUserWithAuthorities();
+		Locale locale = Locale.forLanguageTag(user.get().getLangKey());
+
+		
+		try{
+			
+			//Page<MenuDTO> page = menuService.findAll(pageable);
+			Page <MenuDTO>  page = menuService.buscarAllByUser(user.get().getId(), pageable);
+			HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+			return ResponseEntity.ok().headers(headers).body(page.getContent());
+		}
+		catch(Exception e){
+			String det = "";
+			String message = "api.menus.search.error"; //TODO ESTE SE DEBE CONSULTAR DE LOS MESSAGES DEL SISTEMA
+
+			String error = messageSource.getMessage(message, new String[] {det, e.getMessage()}, locale);
+
+			log.error(error);
+
+			//return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body( e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body( new ApiMessage("ERR_99", error));
+
+		}
+	}
+
+
+	//ADR
+	/**
+	 * {@code GET  /menus} : get all the menus.
+	 *
+
+	 * @param pageable the pagination information.
+
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of menus in body.
+	 */
+
+
+	@GetMapping("/menus-all-user-no-page")
+	//public ResponseEntity<List<MenuDTO>> getAllMenusUser(@PathVariable Long id, Pageable pageable) {
+	public ResponseEntity<?> getAllMenusUserNoPage() {
+		log.debug("REST request to get a page of Menus of User");
+		
+		Optional<User> user = userService.getUserWithAuthorities();
+		Locale locale = Locale.forLanguageTag(user.get().getLangKey());
+		try {
 			List<MenuDTO> list = menuService.buscarAllByUserNoPage(user.get().getId());
 			return new ResponseEntity<>(list, HttpStatus.OK);
 		} catch (Exception e) {
-			String det = "UserId: " +(user.isPresent()?user.get().getLogin():"");
-			String message = "Error de proceso: {0}. {1} "; //TODO ESTE SE DEBE CONSULTAR DE LOS MESSAGES DEL SISTEMA
-			String error = MessageFormat.format(message,  det, e.getMessage());
-			
-			log.error(error);
-			
-            //return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body( new ApiMessage("ERR_99", error));
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body( e.getMessage());
-		}
-        
-        
-           
-        
-    }
+			String det = "";
+			String message = "api.menus.search.error"; //TODO ESTE SE DEBE CONSULTAR DE LOS MESSAGES DEL SISTEMA
 
-    
+			String error = messageSource.getMessage(message, new String[] {det, e.getMessage()}, locale);
+
+			log.error(error);
+
+			//return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body( e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body( new ApiMessage("ERR_99", error));
+		}
+
+
+
+
+	}
+
+
 }
