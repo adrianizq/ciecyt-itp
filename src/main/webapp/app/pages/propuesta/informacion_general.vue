69c55ebd5e1a4d8b59b7bbe8f6cdad65d2890949
Jeyson <jeison130@gmail.com>
2020-06-18 23:18:26 -0500

    formulario informacion general de la propuesta

--- src/main/webapp/app/pages/propuesta/informacion_general.vue
+++ src/main/webapp/app/pages/propuesta/informacion_general.vue
@@ -4,7 +4,51 @@
             <menu-lateral></menu-lateral>
         </div>
         <div class="col-sm-8">
-            en información general
+            <form>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label class="form-control-label" for="titulo_proyecto">Titulo del proyecto</label>
+                            <input type="text" class="form-control" name="titulo_proyecto" id="titulo_proyecto" />
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label class="form-control-label" for="modalidad">Modalidad</label>
+                            <input type="text" class="form-control" name="modalidad" id="modalidad" />
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label class="form-control-label" for="facultad">Facultad</label>
+                            <input type="text" class="form-control" name="facultad" id="facultad" />
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label class="form-control-label" for="linea_investigacion">Linea de investigación</label>
+                            <input type="text" class="form-control" name="linea_investigacion" id="linea_investigacion" />
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label class="form-control-label" for="sublinea">Sub linea</label>
+                            <input type="text" class="form-control" name="sublinea" id="sublinea" />
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label class="form-control-label" for="asesor">Asesor</label>
+                            <input type="text" class="form-control" name="asesor" id="asesor" />
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </template>
