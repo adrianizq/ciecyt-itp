713d4ce72025edf67e4ac42deb265cb78db3efe6
Jeyson <jeison130@gmail.com>
2020-07-08 20:27:45 -0500

    Guardando información proyecto

--- src/main/webapp/app/pages/propuesta/informacion_general.vue
+++ src/main/webapp/app/pages/propuesta/informacion_general.vue
@@ -4,30 +4,30 @@
             <menu-lateral></menu-lateral>
         </div>
         <div class="col-sm-8">
-            <form>
+            <form @submit.prevent="save()">
                 <div class="row">
                     <div class="col-12">
-                       <!-- Jeison 
-                        <div class="form-group">
-                            <label class="form-control-label" for="titulo_proyecto">Titulo del proyecto</label>
-                            <input type="text" class="form-control" name="titulo_proyecto" id="titulo_proyecto"
-                            v-model=proyecto.titulo />
-                        </div>
-                        -->
+                        <!-- Jeison
+                         <div class="form-group">
+                             <label class="form-control-label" for="titulo_proyecto">Titulo del proyecto</label>
+                             <input type="text" class="form-control" name="titulo_proyecto" id="titulo_proyecto"
+                             v-model=proyecto.titulo />
+                         </div>
+                         -->
 
                         <div class="form-group" v-if="proyecto.id">
                             <label for="id" v-text="$t('global.field.id')">ID</label>
                             <input type="text" class="form-control" id="id" name="id"
-                               v-model="proyecto.id" readonly />
+                                   v-model="proyecto.id" readonly/>
                         </div>
+
                         <div class="form-group">
                             <label class="form-control-label" v-text="$t('ciecytApp.proyecto.titulo')" for="proyecto-titulo">Titulo</label>
                             <input type="text" class="form-control" name="titulo" id="proyecto-titulo"
-                             v-model="proyecto.titulo" />
+                                   v-model="proyecto.titulo"/>
                         </div>
 
-                   
-                    
+
                     </div>
 
                     <div class="col-md-6 col-12">
@@ -35,7 +35,7 @@
                             label="Modalidad"
                             label-for="modalidad"
                         >
-                            <b-form-select :options="modalidads" text-field="modalidad" value-field="id" id="modalidad">
+                            <b-form-select :options="modalidads" text-field="modalidad" value-field="id" id="modalidad" v-model="proyecto.proyectoModalidadId">
 
                             </b-form-select>
                         </b-form-group>
@@ -46,7 +46,7 @@
                             label="Facultad"
                             label-for="facultad"
                         >
-                            <b-form-select :options="facultades" v-model="facultad" text-field="facultad" value-field="id" id="facultad">
+                            <b-form-select :options="facultades" v-model="proyecto.facultadId" text-field="facultad" value-field="id" id="facultad">
 
                             </b-form-select>
                         </b-form-group>
@@ -57,7 +57,7 @@
                             label="Linea de investigación"
                             label-for="linea_investigacion"
                         >
-                            <b-form-select text-field="linea" v-model="linea_investigacion" value-field="id" id="linea_investigacion">
+                            <b-form-select text-field="linea" v-model="proyecto.proyectoLineaInvestigacionId" value-field="id" id="linea_investigacion">
                                 <option v-for="(selectOption, indexOpt) in LineasInvestigacion"
                                         :key="indexOpt"
                                         :value="selectOption.id"
@@ -73,7 +73,7 @@
                             label="Sub línea"
                             label-for="sub_linea"
                         >
-                            <b-form-select text-field="linea" value-field="id" id="linea_investigacion">
+                            <b-form-select text-field="linea" value-field="id" id="linea_investigacion" >
                                 <option v-for="(selectOption, indexOpt) in SubLineas"
                                         :key="indexOpt"
                                         :value="selectOption.id"
@@ -83,18 +83,18 @@
                             </b-form-select>
                         </b-form-group>
                     </div>
-                 
+
                     <!-- ADR   -->
-                      <div class="col-md-6 col-12">
+                    <div class="col-md-6 col-12">
                         <b-form-group
                             label="Asesor"
                             label-for="usuario"
                         >
                             <!--
                             <b-form-select :options="users" v-model="user" text-field="firstName" value-field="id" id="user">
-                             -->    
-                            <b-form-select :options="users" v-model="user" text-field = "nombresApellidos"  value-field="id" id="user">
-                               
+                             -->
+                            <b-form-select :options="users" v-model="user" text-field="nombresApellidos" value-field="id" id="user" >
+
                             </b-form-select>
                         </b-form-group>
                     </div>
@@ -102,25 +102,25 @@
 
                 </div>
 
-<!-- 
+                <!--
+                                <div>
+                                    <button type="button" id="cancel-save" class="btn btn-secondary" v-on:click="previousState()">
+                                        <font-awesome-icon icon="ban"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.cancel')">Cancel</span>
+                                    </button>
+                                     <button type="submit" id="save-entity" :disabled="$v.proyecto.$invalid || isSaving" class="btn btn-primary">
+                                        <font-awesome-icon icon="save"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.save')">Save</span>
+                                    </button>
+                -->
                 <div>
+
                     <button type="button" id="cancel-save" class="btn btn-secondary" v-on:click="previousState()">
                         <font-awesome-icon icon="ban"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.cancel')">Cancel</span>
                     </button>
-                     <button type="submit" id="save-entity" :disabled="$v.proyecto.$invalid || isSaving" class="btn btn-primary">
-                        <font-awesome-icon icon="save"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.save')">Save</span>
-                    </button>
--->
-                <div>
-                    
-                    <button type="button" id="cancel-save" class="btn btn-secondary" v-on:click="previousState()">
-                        <font-awesome-icon icon="ban"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.cancel')">Cancel</span>
-                    </button>
-                    <button type="submit" id="save-entity"  class="btn btn-primary">
+                    <button type="submit" id="save-entity" class="btn btn-primary">
                         <font-awesome-icon icon="save"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.save')">Save</span>
                     </button>
 
-                    
+
                 </div>
 
             </form>
@@ -129,8 +129,6 @@
 </template>
 
 <script lang="ts">
-    //import Component from 'vue-class-component';
-    //import { Inject, Vue } from 'vue-property-decorator';
     import { Component, Inject, Vue } from 'vue-property-decorator';
     import AlertService from '@/shared/alert/alert.service';
 
@@ -146,33 +144,27 @@
     import { IUser } from '@/shared/model/user.model';
     //ADR
     import { IProyecto, Proyecto } from '@/shared/model/proyecto.model';
-    import ProyectoService from  '@/entities/proyecto/proyecto.service';
-import { numeric, required, minLength, maxLength } from 'vuelidate/lib/validators';
+    import ProyectoService from '@/entities/proyecto/proyecto.service';
+    import { numeric, required, minLength, maxLength } from 'vuelidate/lib/validators';
 
-const validations: any = {
-  proyecto: {
-    titulo: {},
-    url: {},
-    lugarEjecucion: {},
-    duracion: {},
-    fechaIni: {},
-    fechaFin: {},
-    contrapartidaPesos: {},
-    contrapartidaEspecie: {},
-    palabrasClave: {},
-    convocatoria: {}
-  }
-};
-
-@Component({
-  validations
-})
-
-    
+    const validations: any = {
+        proyecto: {
+            titulo: {},
+            url: {},
+            lugarEjecucion: {},
+            duracion: {},
+            fechaIni: {},
+            fechaFin: {},
+            contrapartidaPesos: {},
+            contrapartidaEspecie: {},
+            palabrasClave: {},
+            convocatoria: {}
+        }
+    };
 
     @Component({
-        components: { MenuLateral }
-        
+        components: { MenuLateral },
+        validations
     })
 
     export default class PropuestaInformacionGeneral extends Vue {
@@ -181,8 +173,7 @@ const validations: any = {
         @Inject('lineaInvestigacionService') private lineaInvestigacionService: () => LineaInvestigacionService;
         @Inject('usuarioService') private usuarioService: () => UsuarioService;
         @Inject('proyectoService') private proyectoService: () => ProyectoService;
-         @Inject('alertService') private alertService: () => AlertService;
-         
+        @Inject('alertService') private alertService: () => AlertService;
 
         public modalidads: IModalidad[] = [];
         public facultades: IFacultad[] = [];
@@ -190,13 +181,13 @@ const validations: any = {
         //public users: IUsuario[] = [];
         public users: IUser[] = [];
 
-        public linea_investigacion: number = null
-        public facultad: number = null
-        public user: number = null
-        public nombresApellidos: string = null
+        public linea_investigacion: number = null;
+        public facultad: number = null;
+        public user: number = null;
+        public nombresApellidos: string = null;
         public proyecto: IProyecto = new Proyecto();
 
-     public isSaving = false;
+        public isSaving = false;
 
         beforeRouteEnter(to, from, next) {
             next(vm => {
@@ -225,33 +216,26 @@ const validations: any = {
             this.proyectoService()
                 .create(this.proyecto)
                 .then(param => {
-                this.isSaving = false;
-                this.$router.go(-1);
-                const message = this.$t('ciecytApp.proyecto.created', { param: param.id });
-                this.alertService().showAlert(message, 'success');
+                    this.isSaving = false;
+                    this.$router.go(-1);
+                    const message = this.$t('ciecytApp.proyecto.created', { param: param.id });
+                    this.alertService().showAlert(message, 'success');
                 });
             //}
-  }
-
-
-        get LineasInvestigacion(){
-            return this.lineas_investigacion.filter(linea => {
-                return (!linea.lineaPadreId && linea.lineaInvestigacionFacultadId == this.facultad)
-            })
         }
 
-        get SubLineas(){
+        get LineasInvestigacion() {
             return this.lineas_investigacion.filter(linea => {
-                return (linea.lineaPadreId == this.linea_investigacion && linea.lineaPadreId)
-            })
+                return (!linea.lineaPadreId && linea.lineaInvestigacionFacultadId == this.proyecto.facultadId);
+            });
         }
 
-        //get NombresApellidos(){
-        //      return this.user.firstName + " " + this.user.lastName
-              
-        //    }
+        get SubLineas() {
+            return this.lineas_investigacion.filter(linea => {
+                return (linea.lineaPadreId == this.proyecto.proyectoLineaInvestigacionId && linea.lineaPadreId);
+            });
+        }
 
-        
         initRelationships() {
             //Obtenienedo las modalidades
             this.modalidadService()
@@ -271,22 +255,20 @@ const validations: any = {
             this.lineaInvestigacionService()
                 .retrieve()
                 .then(res => {
-                    this.lineas_investigacion = res.data
+                    this.lineas_investigacion = res.data;
                 });
 
             //Obteniendo los usuarios asesores
             this.usuarioService()
                 .retrieveAsesores()
                 .then(res => {
-                    //this.users = res.data;
-                    //console.log(res.data)
-                     res.data.forEach((item) => { 
-                     item.nombresApellidos = item.firstName + " " + item.lastName 
-                     this.users.push(item);
-                     })
-                     //console.log(this.users)
+                    res.data.forEach((item) => {
+                        item.nombresApellidos = item.firstName + ' ' + item.lastName;
+                        this.users.push(item);
+                    });
+
                 });
-            
+
         }
     }
 </script>
