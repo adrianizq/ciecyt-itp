a465ae75b704147ae7e069b73eb36d49de4146db
Jeyson <jeison130@gmail.com>
2020-07-17 22:27:29 -0500

    Merge integrantes

--- src/main/webapp/app/pages/propuesta/integrantes.vue
+++ src/main/webapp/app/pages/propuesta/integrantes.vue
@@ -4,26 +4,51 @@
             <menu-lateral></menu-lateral>
         </div>
         <div class="col-sm-8">
-            <form @submit.prevent="save()"> 
-            <div class="row">
-            En integrantes
+            <form @submit.prevent="save()">
+                <div class="row">
 
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label class="form-control-label" for="proyecto-documento">Documento</label>
+                                <input type="text" class="form-control" name="titulo" id="proyecto-documento"
+                                />
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label class="form-control-label" for="proyecto-nombre">Nombres</label>
+                                <input type="text" class="form-control" name="titulo" id="proyecto-nombre"
+                                />
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label class="form-control-label" for="proyecto-apellido">Apellidos</label>
+                                <input type="text" class="form-control" name="titulo" id="proyecto-apellido"
+                                />
+                            </div>
+                            <button type="submit" id="save-entity" class="btn btn-primary">
+                                <font-awesome-icon icon="save"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.save')">Save</span>
+                            </button>
+                        </div>
 
-             <!-- ADR   -->
-                <div class="col-md-6 col-12">
-                    <b-form-group
-                        label="Asesor"
-                        label-for="usuario"
-                    >
-                        <b-form-select :options="users" v-model="proyecto.asesorId" text-field="nombresApellidos" value-field="id" id="proyecto-asesorId" >
+                        <!-- ADR   -->
+                        <div class="col-md-6 col-12">
+                            <b-form-group
+                                label="Asesor"
+                                label-for="usuario"
+                            >
+                                <b-form-select :options="users" v-model="proyecto.asesorId" text-field="nombresApellidos" value-field="id" id="proyecto-asesorId">
 
-                        </b-form-select>
-                    </b-form-group>
+                                </b-form-select>
+                            </b-form-group>
+                        </div>
+                        <div class="col-md-6 col-12">
+                            ROLES DE MODALIDAD
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-6 col-12">
-                    ROLES DE MODALIDAD
-                </div>
-            </div> 
             </form>
 
         </div>
@@ -36,18 +61,17 @@
     import AlertService from '@/shared/alert/alert.service';
     import UsuarioService from '@/entities/usuario/usuario.service';
     import { IUser } from '@/shared/model/user.model';
-   
 
     @Component({
         components: { MenuLateral }
     })
     export default class Integrantes extends Vue {
 
-         @Inject('usuarioService') private usuarioService: () => UsuarioService;
+        @Inject('usuarioService') private usuarioService: () => UsuarioService;
 
-         public users: IUser[] = [];
+        public users: IUser[] = [];
 
-         beforeRouteEnter(to, from, next) {
+        beforeRouteEnter(to, from, next) {
             next(vm => {
                 if (to.params.proyectoId) {
                     vm.retrieveProyecto(to.params.proyectoId);
@@ -58,12 +82,12 @@
                 }*/
                 vm.initRelationships();
 
-                console.log('Carga usuarios...'+this.users);
+                console.log('Carga usuarios...' + this.users);
             });
         }
 
         initRelationships() {
-            
+
             //Obteniendo los usuarios asesores
             this.usuarioService()
                 .retrieveAsesores()
