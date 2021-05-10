262d9920b0133c2dcb53d3cdc4285f20b7828b83
ALVARO ADRIAN IZQUIERDO GOMEZ <aizquierdo@itp.edu.co>
2021-05-10 10:27:20 -0500

    dando permisos para los menus en java

--- src/main/java/co/edu/itp/ciecyt/config/SecurityConfiguration.java
+++ src/main/java/co/edu/itp/ciecyt/config/SecurityConfiguration.java
@@ -79,6 +79,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
+            .antMatchers("/api/menus-no-page").permitAll()
+            .antMatchers("/api/menu**").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/**").authenticated()
