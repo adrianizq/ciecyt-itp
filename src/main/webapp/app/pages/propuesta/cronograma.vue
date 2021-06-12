145b79eac7101972bdf193ed4acc0b5e5c8097e7
ALVARO ADRIAN IZQUIERDO GOMEZ <aizquierdo@itp.edu.co>
2021-06-11 22:33:50 -0500

    mejorando diplomado

--- src/main/webapp/app/pages/propuesta/cronograma.vue
+++ src/main/webapp/app/pages/propuesta/cronograma.vue
@@ -156,13 +156,13 @@ public cronograms: ICronograma[] = [];
             
                     if (e.id) {
                         this.cronogramaService().update(e); //envio un elemento
-                        this.$router.push({ name: 'PropuestaEnviarPropuestaView',params:{ proyectoId: this.proyId}});
+                        this.$router.push({ name: 'PropuestaAdjuntarPropuestaView',params:{ proyectoId: this.proyId}});
 
                     } else {
                         
                         this.cronogramaService().create(e)
                         .then(param => {
-                            this.$router.push({ name: 'PropuestaEnviarPropuestaView',params:{ proyectoId: this.proyId}});
+                            this.$router.push({ name: 'PropuestaAdjuntarPropuestaView',params:{ proyectoId: this.proyId}});
                             //const message = this.$t('ciecytApp.cronograma.created', { param: param.id });
                             const message = "Se ha creado un nuevo cronograma" + { param: param.id };
                             this.alertService().showAlert(message, 'success');
