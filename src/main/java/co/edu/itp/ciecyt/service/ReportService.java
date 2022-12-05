9ace94bf1fb3e0468f57f6e0e2652b2454e25898
adrianizq <adrianizq@gmail.com>
2022-12-04 23:49:58 -0500

    quitando lo de japster

--- src/main/java/co/edu/itp/ciecyt/service/ReportService.java
+++
@@ -1,9 +0,0 @@
-package co.edu.itp.ciecyt.service;
-
-import net.sf.jasperreports.engine.JRException;
-
-import java.io.FileNotFoundException;
-
-public interface ReportService {
-    public String exportReport (String reportFormat) throws FileNotFoundException, JRException;
-}
